a89feac79f322cc9d4c49d73912aef2603ec6b7d
tmittal2804 <tmittal6960@gmail.com>
2025-09-19 01:12:13 +0530

    Code

--- Input,Output &Variables/Addition.c
+++ Input,Output &Variables/Addition.c
@@ -2,12 +2,12 @@
 
 #include<stdio.h>
 int main () {
-int a,b;
-printf ("Enter the first number:");
-scanf("%d", &a);
-printf ("Enter the second  number:");
-scanf("%d", &b);
-printf("The sum of two  numbers is %d\n",a+b);
-return 0;
-}
+    int a,b;
+    printf ("Enter the first number:");
+    scanf("%d", &a);
+    printf ("Enter the second  number:");
+    scanf("%d", &b);
+    printf("The sum of two  numbers is %d\n",a+b);
+    return 0;
+    }
 
