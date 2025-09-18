50b641c51763d0fc6d5a13899968562180960799
tmittal2804 <tmittal6960@gmail.com>
2025-09-19 01:14:29 +0530

    Code

---
+++ Input,Output &Variables/Circle.c
@@ -0,0 +1,11 @@
+// Read the radius of a circle and print its area (use π = 3.14).
+
+#include <stdio.h>
+int main () {
+    float radius;
+    printf("Enter the radius:");
+    scanf("%f", &radius);
+    printf("Area  of Circle is %0.2f\n",3.14*radius*radius);
+    return 0;
+}
+
