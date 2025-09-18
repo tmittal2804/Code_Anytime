5eef3728ec67ce4821932b8d43b059bb6b54fdae
tmittal2804 <tmittal6960@gmail.com>
2025-09-19 01:13:26 +0530

    Code

---
+++ Input,Output &Variables/fahrenhiet.c
@@ -0,0 +1,12 @@
+// Convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+int main() {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+    return 0;
+    }
+
