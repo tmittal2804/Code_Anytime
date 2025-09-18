384c09fcba0b0def6cd053e5855c097ac1783503
tmittal2804 <tmittal6960@gmail.com>
2025-09-18 23:26:02 +0530

    Code

---
+++ Input,Output &Variables/Addition.c
@@ -0,0 +1,13 @@
+// Read two integers from user input and display their sum.
+
+#include<stdio.h>
+int main () {
+int a,b;
+printf ("Enter the first number:");
+scanf("%d", &a);
+printf ("Enter the second  number:");
+scanf("%d", &b);
+printf("The sum of two  numbers is %d\n",a+b);
+return 0;
+}
+
