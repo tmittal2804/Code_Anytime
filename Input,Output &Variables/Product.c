c1d58d4fe1d64b375d3389216cc63de424865f52
tmittal2804 <tmittal6960@gmail.com>
2025-09-19 01:16:26 +0530

    Code

--- Input,Output &Variables/Product.c
+++ Input,Output &Variables/Product.c
@@ -2,12 +2,12 @@
 
 #include<stdio.h>
 int main () {
-float a,b;
-printf("Enter the first number:");
-scanf("%f",  &a);
-printf("Enter the second number:");
-scanf("%f", &b);
-printf("The product of two number is %0.2f\n",a*b);
-return 0;
+    float a,b;
+    printf("Enter the first number:");
+    scanf("%f",  &a);
+    printf("Enter the second number:");
+    scanf("%f", &b);
+    printf("The product of two number is %0.2f\n",a*b);
+    return 0;
 }
  
