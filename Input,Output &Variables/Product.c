9a98136a3baba0da21f14f5d7af4b1fa0fa77448
tmittal2804 <tmittal6960@gmail.com>
2025-09-18 23:41:43 +0530

    code

---
+++ Input,Output &Variables/Product.c
@@ -0,0 +1,13 @@
+// Input two floating-point numbers and print their product.
+
+#include<stdio.h>
+int main () {
+float a,b;
+printf("Enter the first number:");
+scanf("%f",  &a);
+printf("Enter the second number:");
+scanf("%f", &b);
+printf("The product of two number is %0.2f\n",a*b);
+return 0;
+}
+ 
