19cf78a167b5beff470129422bb6227b322aee10
tmittal2804 <tmittal6960@gmail.com>
2025-09-18 23:25:23 +0530

    Code

---
+++ Input,Output &Variables/Name.c
@@ -0,0 +1,12 @@
+// Accept a user’s name and print a message: “Hello, [Name]!”.
+
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%49s", name);
+    printf("Hello, %s!\n", name);
+    return 0;
+}
+
